2bb7ddd57a34f39a915c13e06e5e209409a507d4
Colleen Mills <comills@ucdavis.edu>
2021-10-04 13:17:53 -0700

    More vector practice

---
+++ Vector_Practice2.R
@@ -0,0 +1,20 @@
+#Vector Practice 2
+
+Vector1<-c(3,2,1)
+Vector1
+
+Vector1[c(2,1,1)]
+data.frame(Vector1)
+
+(test_list<-('first string entry into list'))
+test_list[[1]][2]<-'add next thing'
+test_list
+test_df<-data.frame('letters','letters')
+str(test_df)
+head(test_df)
+?head()
+
+test_df2<-data.frame('stuff','more stuff','even more stuff')
+str(test_df2)
+?str()
+head(test)
