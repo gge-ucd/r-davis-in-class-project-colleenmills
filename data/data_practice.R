3436a56b0917c277ae6656b86125fe82a980a887
Colleen Mills <comills@ucdavis.edu>
2021-10-08 12:43:12 -0700

    committed

--- data/data_practice.R
+++ data/data_practice.R
@@ -26,3 +26,15 @@ species_factor<-factor(surveys$species)
 typeof(species_factor)
 class(species_factor)
 levels(species_factor)
+
+surveys_200<-data.frame(surveys[200,])
+surveys_200
+surveys_last<-data.frame(surveys[nrow(surveys),])
+surveys_last
+
+surveys_length<-nrow(surveys)
+surveys_length
+class(surveys_length)                 
+surveys_middlerow<-surveys_length/2
+surveys_middlerow
+middlerow->surveys[surveys_middlerow,]
