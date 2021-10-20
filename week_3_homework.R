71fcb3189322ddc12088dc46adb41bce34909159
Colleen Mills <comills@ucdavis.edu>
2021-10-19 21:33:25 -0700

    Another commit message

---
+++ week_3_homework.R
@@ -0,0 +1,19 @@
+#************************#
+#Week 3 Homework
+#
+#Author: Colleen Mills
+#
+#comills@ucdavis.edu
+#
+#October 19, 2021
+#************************#
+
+#Homework Week 3
+#install.packages("tidyverse")
+library(tidyverse)
+surveys<- read.csv("data/portal_data_joined.csv")
+?select
+surveys_base_in_progress<-select(surveys,species_id,weight,plot_type)
+surveys_base_in_progress
+surveys_base<-surveys_base_in_progress[1:60,]
+surveys_base
