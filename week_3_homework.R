e0e7c4f945ed7fcf835a101ef16843f8c3052dfa
Colleen Mills <comills@ucdavis.edu>
2021-10-19 22:24:20 -0700

    Commit again!

--- week_3_homework.R
+++ week_3_homework.R
@@ -17,3 +17,11 @@ surveys_base_in_progress<-select(surveys,species_id,weight,plot_type)
 surveys_base_in_progress
 surveys_base<-surveys_base_in_progress[1:60,]
 surveys_base
+str(surveys_base)
+?factor
+surveys_base[,'species_id']<-as.factor(surveys_base[,'species_id'])
+str(surveys_base)
+surveys_base %>% select(surveys_base,!is.na(weight))
+
+#CHALLENGE
+challenge_base<-surveys_base %>%filter(weight>150)
