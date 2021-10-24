73f33268b884148abc52788d5f25b8bb0f58f6c8
Colleen Mills <comills@ucdavis.edu>
2021-10-24 14:27:01 -0700

    Second commit...I think...

--- week_4_homework.R
+++ week_4_homework.R
@@ -9,3 +9,18 @@
 #************************#
 
 #Homework Week 4
+library(tidyverse)
+surveys<-read.csv("data/portal_data_joined.csv")
+surveys
+surveys <- surveys %>% filter(weight >= 30 & weight <= 60) %>% head()
+surveys
+surveys <- read.csv("data/portal_data_joined.csv") %>% filter(!is.na(weight))
+surveys
+biggest_critters <- surveys %>% group_by(species_id,sex) %>% summarize(max_weight = max(weight))
+biggest_critters
+biggest_critters <- surveys %>% group_by(species_id,sex) %>% summarize(min_weight = min(weight), max_weight = max(weight))
+biggest_critters
+?arrange
+?tally
+where_are_NAs <- tally(surveys,is.na(weight))
+where_are_NAs
