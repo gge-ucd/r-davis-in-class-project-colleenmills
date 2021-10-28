9a094d9b682100e2d194fd4bb6d17ebaf42b20c2
Colleen Mills <comills@ucdavis.edu>
2021-10-27 22:24:07 -0700

    First commit and halfway there

---
+++ week_5_homework.R
@@ -0,0 +1,13 @@
+#************************#
+#Week 5 Homework
+#
+#Author: Colleen Mills
+#
+#comills@ucdavis.edu
+#
+#October 25, 2021
+#************************#
+
+#Week 5 Homework
+library(tidyverse)
+surveys<-read.csv("data/portal_data_joined.csv")
