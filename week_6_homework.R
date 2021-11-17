0161f15b93fff8dc4d81683a8d371bf59f74c50d
Colleen Mills <comills@ucdavis.edu>
2021-11-16 16:32:25 -0800

    Close on problem 1.

--- week_6_homework.R
+++ week_6_homework.R
@@ -13,3 +13,4 @@ gapminder <- read_csv("https://gge-ucd.github.io/R-DAVIS/data/gapminder.csv")
 head(gapminder)
 
 #Problem 1
+gapminder<-gapminder%>%group_by(continent,lifeExp)%>%summarize(mean_lifeExp=mean(lifeExp))%>%ggplot(data=gapminder,mapping=aes(x=continent,y=mean_lifeExp)) + geom_point()
