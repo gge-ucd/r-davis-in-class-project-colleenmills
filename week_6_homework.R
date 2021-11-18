6615bc9f8e3d489e793454bd9ce157f9ae9347e3
Colleen Mills <comills@ucdavis.edu>
2021-11-18 15:02:25 -0800

    Problem 2 is done!

--- week_6_homework.R
+++ week_6_homework.R
@@ -14,5 +14,27 @@ head(gapminder)
 
 #Problem 1
 
-gapminder<-gapminder%>%group_by(continent,year)%>%summarize(mean_lifeExp=mean(lifeExp))%>%ggplot()+geom_point(mapping=aes(x=year,y=mean_lifeExp,color=continent))+geom_line(mapping=aes(x=year,y=mean_lifeExp,color=continent))
-                                                                                                              
+New_Plot<-gapminder%>%group_by(continent,year)%>%summarize(mean_lifeExp=mean(lifeExp))%>%
+  ggplot()+
+  geom_point(mapping=aes(x=year,y=mean_lifeExp,color=continent))+geom_line(mapping=aes(x=year,y=mean_lifeExp,color=continent))
+New_Plot
+
+#Problem 2
+
+#My first guess is that scale_x_log10() creates a log10 scale on the x-axis.
+#My other guess is that geom_smooth creates a smooth plot line that is curved rather than creating smooth lines between points
+
+#Now, we shall test it.
+
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(aes(color = continent), size = .25) + 
+  scale_x_log10() +
+  geom_smooth(method = 'lm', color = 'black', linetype = 'dashed') +
+  theme_bw()
+
+?scale_x_log10
+?geom_smooth
+#It turns out that scale_x_log10() does seem to log transform the x-axis.
+#geom_smooth creates what appears to be a best-fit line through the data points
+
+#Problem 3
