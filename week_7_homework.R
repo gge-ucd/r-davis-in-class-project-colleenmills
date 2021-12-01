2623178a00f2ec40c06af51c127e82be8c22d02f
Colleen Mills <comills@ucdavis.edu>
2021-12-01 13:43:24 -0800

    Got further...

--- week_7_homework.R
+++ week_7_homework.R
@@ -8,3 +8,9 @@
 #November 30, 2021
 #************************#
 library(tidyverse)
+gapminder <- read_csv("https://gge-ucd.github.io/R-DAVIS/data/gapminder.csv")
+
+Great.plot<-gapminder%>%select(continent,country,year,pop)%>%filter(year>2000)%>%pivot_wider(names_from=year,values_from=pop)%>%
+  mutate(pop_diff=`2007`-`2002`)
+Another.great.plot<-Great.plot%>%mutate(pop_diff="2007"-"2002")
+head(Great.plot)
