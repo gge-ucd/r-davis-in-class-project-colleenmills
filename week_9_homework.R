8c1b82f71f2add9d4744dbd0eb6bb65db9b87688
Colleen Mills <comills@ucdavis.edu>
2021-12-01 21:16:56 -0800

    Lots of parameters! Let's go!

--- week_9_homework.R
+++ week_9_homework.R
@@ -7,3 +7,28 @@
 #
 #December 01, 2021
 #************************#
+#Notes
+#brw = Barrow, Alaska - Alaska Standard Time (GMT-9)
+#mlo = Mauna Loa, Hawaii - Hawaiian-Aleutian Standard Time (GMT-10)
+#smo = American Samoa - Samoa Standard Time (GMT-11)
+#sum = Summit, Greenland - (GMT-3)
+#thd = Trinidad Head, California - Pacific Standard Time (GMT-8)
+#spo = South Pole, Antarctica
+
+#Missing Values
+#6: Wind Direction -999
+#7: Wind Speed -999.9
+#8: Wind Steadiness Factor -9
+#9: Barometric Pressure -999.90
+#10: Temperature at 2 Meters -999.9
+#11: Temperature at 10 Meters -999.9
+#12: Temperature at Tower Top -999.9
+#13: Relative humidity -99
+#14: Precipitation intensity -99
+
+install.packages('lubridate')
+library(lubridate)
+mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
+
+head(mloa)
+
