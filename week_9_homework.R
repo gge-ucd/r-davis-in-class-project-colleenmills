069abd4e7e1123607d235717ba35ccc3c5822b23
Colleen Mills <comills@ucdavis.edu>
2021-12-02 15:38:04 -0800

    So close!

--- week_9_homework.R
+++ week_9_homework.R
@@ -39,4 +39,17 @@ mloa2<-mloa%>%filter(rel_humid!=-99,temp_C_2m!=-999.99,windSpeed_m_s!=-999.9)
 head(mloa2)
 
 #Generate column called "datetime" using year, month, day, hour24, and min columns
-mloa3<-mloa2%>
+mloa2$datetime<-paste(mloa2$year," ",mloa2$month," ",mloa2$day," ",mloa2$hour24," ",mloa2$min,sep="")
+mloa2$datetime
+
+#Create new column called "datetimeLocal" that converts times to Honolulu/Pacific time
+mloa2$datetime<-ymd_hm(mloa2$datetime)
+mloa2$datetime
+mloa2$datetimeLocal<-as_datetime(mloa2$datetime,tz="Pacific/Honolulu")
+mloa2$datetimeLocal
+
+#Calculate mean temperature each month using temp_C_2m and the datetimeLocal column
+?month()
+months<-month(mloa2$datetimeLocal)
+hours<-hour(mloa2$datetimeLocal)
+mloa2$meanMonthlyTempByHour<-mloa2%>%group_by(months)
